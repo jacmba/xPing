9f815a5fd65eef78804abaac5dbb9bed8386c9ed
Jacinto Mba Cantero <jacmba@gmail.com>
2025-02-13 00:54:47 +0000

    Just attempting stuff

--- xPing/xPing.cpp
+++ xPing/xPing.cpp
@@ -1,8 +1,11 @@
-// Downloaded from https://developer.x-plane.com/code-sample/hello-world-sdk-3/
-
 #include "XPLMDisplay.h"
 #include "XPLMGraphics.h"
+#include "XPWidgets.h"
+#include "XPStandardWidgets.h"
+#include "XPWidgetUtils.h"
+#include "XPLMUtilities.h"
 #include <string.h>
+#include <string>
 #if IBM
 	#include <windows.h>
 #endif
@@ -18,93 +21,65 @@
 	#error This is made to be compiled against the XPLM300 SDK
 #endif
 
-// An opaque handle to the window we will create
-static XPLMWindowID	g_window;
+static XPLMWindowID g_window = NULL;
+static XPWidgetID g_widget_window = NULL;
+static XPWidgetID g_button = NULL;
 
-// Callbacks we will register when we create our window
-void				draw_hello_world(XPLMWindowID in_window_id, void * in_refcon);
-int					dummy_mouse_handler(XPLMWindowID in_window_id, int x, int y, int is_down, void * in_refcon) { return 0; }
-XPLMCursorStatus	dummy_cursor_status_handler(XPLMWindowID in_window_id, int x, int y, void * in_refcon) { return xplm_CursorDefault; }
-int					dummy_wheel_handler(XPLMWindowID in_window_id, int x, int y, int wheel, int clicks, void * in_refcon) { return 0; }
-void				dummy_key_handler(XPLMWindowID in_window_id, char key, XPLMKeyFlags flags, char virtual_key, void * in_refcon, int losing_focus) { }
+void draw_window(XPLMWindowID in_window_id, void *in_refcon);
+int widget_handler(XPWidgetMessage message, XPWidgetID widgetId, intptr_t param1, intptr_t param2);
 
-PLUGIN_API int XPluginStart(
-							char *		outName,
-							char *		outSig,
-							char *		outDesc)
-{
-	strcpy(outName, "xPing");
-	strcpy(outSig, "net.jazbelt.xping");
-	strcpy(outDesc, "ACARS plugin with CPDLC capabilities");
-	
-	XPLMCreateWindow_t params;
-	params.structSize = sizeof(params);
-	params.visible = 1;
-	params.drawWindowFunc = draw_hello_world;
-	// Note on "dummy" handlers:
-	// Even if we don't want to handle these events, we have to register a "do-nothing" callback for them
-	params.handleMouseClickFunc = dummy_mouse_handler;
-	params.handleRightClickFunc = dummy_mouse_handler;
-	params.handleMouseWheelFunc = dummy_wheel_handler;
-	params.handleKeyFunc = dummy_key_handler;
-	params.handleCursorFunc = dummy_cursor_status_handler;
-	params.refcon = NULL;
-	params.layer = xplm_WindowLayerFloatingWindows;
-	// Opt-in to styling our window like an X-Plane 11 native window
-	// If you're on XPLM300, not XPLM301, swap this enum for the literal value 1.
-	params.decorateAsFloatingWindow = xplm_WindowDecorationRoundRectangle;
-	
-	// Set the window's initial bounds
-	// Note that we're not guaranteed that the main monitor's lower left is at (0, 0)...
-	// We'll need to query for the global desktop bounds!
-	int left, bottom, right, top;
-	XPLMGetScreenBoundsGlobal(&left, &top, &right, &bottom);
-	params.left = left + 50;
-	params.bottom = bottom + 150;
-	params.right = params.left + 200;
-	params.top = params.bottom + 200;
-	
-	g_window = XPLMCreateWindowEx(&params);
-	
-	// Position the window as a "free" floating window, which the user can drag around
-	XPLMSetWindowPositioningMode(g_window, xplm_WindowPositionFree, -1);
-	// Limit resizing our window: maintain a minimum width/height of 100 boxels and a max width/height of 300 boxels
-	XPLMSetWindowResizingLimits(g_window, 200, 200, 300, 300);
-	XPLMSetWindowTitle(g_window, "Sample Window");
-	
-	return g_window != NULL;
+PLUGIN_API int XPluginStart(char *outName, char *outSig, char *outDesc) {
+    strcpy(outName, "Widget Test");
+    strcpy(outSig, "net.example.widgettest");
+    strcpy(outDesc, "Testing widgets inside an XPLM window");
+
+    int left, bottom, right, top;
+    XPLMGetScreenBoundsGlobal(&left, &top, &right, &bottom);
+    
+    XPLMCreateWindow_t params = {};
+    params.structSize = sizeof(params);
+    params.visible = 1;
+    params.drawWindowFunc = draw_window;
+    params.left = left + 100;
+    params.bottom = bottom + 100;
+    params.right = params.left + 300;
+    params.top = params.bottom + 200;
+    params.layer = xplm_WindowLayerFloatingWindows;
+    params.decorateAsFloatingWindow = xplm_WindowDecorationRoundRectangle;
+    
+    g_window = XPLMCreateWindowEx(&params);
+
+    g_widget_window = XPCreateWidget(params.left, params.top, params.right, params.bottom, 1, "Widget Window", 1, NULL, xpWidgetClass_MainWindow);
+    g_button = XPCreateWidget(params.left + 50, params.top - 50, params.left + 150, params.top - 100, 1, "Click Me", 0, g_widget_window, xpWidgetClass_Button);
+    XPAddWidgetCallback(g_button, widget_handler);
+    
+    return 1;
 }
 
-PLUGIN_API void	XPluginStop(void)
-{
-	// Since we created the window, we'll be good citizens and clean it up
-	XPLMDestroyWindow(g_window);
-	g_window = NULL;
+PLUGIN_API void XPluginStop(void) {
+    XPDestroyWidget(g_widget_window, 1);
+    XPLMDestroyWindow(g_window);
 }
 
-PLUGIN_API void XPluginDisable(void) { }
-PLUGIN_API int  XPluginEnable(void)  { return 1; }
-PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFrom, int inMsg, void * inParam) { }
+void draw_window(XPLMWindowID in_window_id, void *in_refcon) {
+    int l, t, r, b;
+    XPLMGetWindowGeometry(in_window_id, &l, &t, &r, &b);
+    
+    float bg_color[] = {0.2f, 0.2f, 0.2f, 1.0f};
+    glColor4fv(bg_color);
+    glBegin(GL_QUADS);
+    glVertex2i(l, b);
+    glVertex2i(r, b);
+    glVertex2i(r, t);
+    glVertex2i(l, t);
+    glEnd();
+}
 
-void	draw_hello_world(XPLMWindowID in_window_id, void * in_refcon)
-{
-	// Mandatory: We *must* set the OpenGL state before drawing
-	// (we can't make any assumptions about it)
-	XPLMSetGraphicsState(
-						 0 /* no fog */,
-						 0 /* 0 texture units */,
-						 0 /* no lighting */,
-						 0 /* no alpha testing */,
-						 1 /* do alpha blend */,
-						 1 /* do depth testing */,
-						 0 /* no depth writing */
-						 );
-	
-	int l, t, r, b;
-	XPLMGetWindowGeometry(in_window_id, &l, &t, &r, &b);
-	
-	float col_white[] = {1.0, 1.0, 1.0}; // red, green, blue
-	
-	XPLMDrawString(col_white, l + 10, t - 20, "Hey Ping!", NULL, xplmFont_Proportional);
+int widget_handler(XPWidgetMessage message, XPWidgetID widgetId, intptr_t param1, intptr_t param2) {
+    if (message == xpMsg_PushButtonPressed && widgetId == g_button) {
+        XPLMDebugString("Button clicked!\n");
+        return 1;
+    }
+    return 0;
 }
 
